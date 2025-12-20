b26501556c7f72e5de1f090daef86585451d266b
ferdin vivian <ferdinvivian@gmail.com>
2025-12-21 00:43:10 +0530

    GABI 1st school proj added for 2nd time

---
+++ MCU_Coding/GABI_School/Dec2025_School_Christmas/Core/Inc/Proj_Header.h
@@ -0,0 +1,52 @@
+#include "main.h"
+#include "stm32f4xx_it.h"
+
+#include "stdio.h"
+#include "stdint.h"
+#include "stdlib.h"
+#include "string.h"
+
+#define PC13_DELAY 		100
+#define DF_VOLUME 		30		//0to30
+#define DF_STOP_SEC 	90		
+#define STEP_DELAY 		1			//Min 1
+#define SONG_LOOP 		2			//Min 1
+#define MOTOR_WAIT 		1000	//Min 500
+
+extern TIM_HandleTypeDef htim11;
+extern UART_HandleTypeDef huart1;
+
+
+typedef struct
+{	
+	uint32_t 	Timer_Inc, DF_STOP, DF_REPEAT; 
+	uint32_t	Motor_Wait_Inc, Motor_Step_Inc;
+} GABI_TIMER11_STRUCTURE;
+extern GABI_TIMER11_STRUCTURE Tim11;
+
+typedef struct
+{	
+	uint8_t 	Loop_song, Motor_Step, Song_Play_Status, Motor_status; 
+} PROJ_SOURCE_STRUCTURE;
+extern PROJ_SOURCE_STRUCTURE prj;
+
+void init_function();
+void while_function();
+void tim11_function();
+
+void GABI_SONG_PLAY (void);
+void STEPPER_MOTOR_ON (void);
+void STEPPER_MOTOR_OFF (void);
+
+/*************************************DF PLAYER************************************/
+void Send_cmd (uint8_t cmd, uint8_t Parameter1, uint8_t Parameter2);
+void DF_PlayFromStart(void);
+void DF_Init (uint8_t volume);
+void DF_Next (void);
+void DF_Pause (void);
+void DF_Previous (void);
+void DF_Playback (void);
+
+void Check_Key (void);
+void DF_Reset (void);
+/*************************************DF PLAYER************************************/
---
+++ MCU_Coding/GABI_School/Dec2025_School_Christmas/Core/Src/Project_Source.c
@@ -0,0 +1,141 @@
+#include "Proj_Header.h"
+
+GABI_TIMER11_STRUCTURE 	Tim11;
+PROJ_SOURCE_STRUCTURE 	prj;
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance == TIM11)
+    {
+				tim11_function();
+			
+			  if((prj.Song_Play_Status) && ( Tim11.Motor_Wait_Inc > MOTOR_WAIT))
+				{
+					STEPPER_MOTOR_ON();
+					prj.Motor_status = 1;
+					if((Tim11.Timer_Inc%PC13_DELAY) == 0)
+						HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+				}
+				else
+				{
+					STEPPER_MOTOR_OFF();
+					prj.Motor_status = 0;
+					if((Tim11.Timer_Inc%(PC13_DELAY*8)) == 0)
+						HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+				}
+				
+				if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_10))
+				{
+					Tim11.Motor_Wait_Inc = 0;
+					prj.Song_Play_Status = 0;		//Song Not Playing
+				}
+				else
+				{
+					prj.Song_Play_Status = 1;		//Song Playing
+				}
+
+    }
+}
+
+/*********************************************************************/
+
+void init_function()
+	{
+		prj.Song_Play_Status = prj.Motor_status = 0;
+		STEPPER_MOTOR_OFF();
+		
+		HAL_TIM_Base_Start_IT(&htim11);
+		HAL_Delay(1000);		//DF PLAYER START DELAY, Must
+		
+		DF_Init(DF_VOLUME);
+		DF_PlayFromStart();
+	}
+
+void while_function()
+	{
+		GABI_SONG_PLAY();
+	}
+
+void tim11_function()
+	{
+		Tim11.Timer_Inc+=1;
+		Tim11.DF_STOP+=1;
+		Tim11.DF_REPEAT+=1;
+		Tim11.Motor_Step_Inc+=1;
+		Tim11.Motor_Wait_Inc+=1;
+	}
+
+
+
+void GABI_SONG_PLAY (void)
+{
+		if(Tim11.DF_STOP > (DF_STOP_SEC*1000))
+			{
+					//DF_Reset();
+					DF_Pause();
+					Tim11.DF_STOP = (Tim11.DF_STOP - 5000);
+					//To avoid starting again, doing pause and reset
+			}
+			
+		if(Tim11.DF_REPEAT > 38000)
+			{
+					Tim11.DF_REPEAT = 1000;
+					prj.Loop_song+=1;
+					
+					if(prj.Loop_song < SONG_LOOP)
+							DF_PlayFromStart();
+					else
+							prj.Loop_song = SONG_LOOP;
+			}
+}
+
+
+
+void STEPPER_MOTOR_ON (void)
+{
+	if(Tim11.Motor_Step_Inc > STEP_DELAY)
+	{
+		Tim11.Motor_Step_Inc = 0;
+		switch(prj.Motor_Step)
+		{
+			case 1:
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+				prj.Motor_Step = 2;
+				break;
+			case 2:
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+				prj.Motor_Step = 3;
+				break;
+			case 3:
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+				prj.Motor_Step = 4;
+				break;
+			case 4:
+				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+				HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+				prj.Motor_Step = 1;
+				break;
+			default:
+				prj.Motor_Step = 1;
+				break;
+		}
+	}
+}
+
+
+void STEPPER_MOTOR_OFF (void)
+{
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
+}
